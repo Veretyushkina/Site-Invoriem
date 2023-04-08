0e2dfd7010c8f47915ec93f92a55d44114e08b16
Veretyushkina <toryray13@gmail.com>
2023-04-08 23:53:16 +0300

    added section main

---
+++ js/main.js
@@ -0,0 +1,33 @@
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        dots: false,
+        margin: 48,
+        loop: true
+    });
+  });
+
+  const videoBtn = document.querySelector('#video-btn');
+  const videoPicture = document.querySelector('.video__picture');
+  const videoWrapper = document.querySelector('.video');
+  const video = document.querySelector('#video-file');
+
+  videoBtn.addEventListener('click', function (){
+        videoPicture.classList.add('none');
+        videoWrapper.classList.remove('video-overlay');
+        videoBtn.classList.add('none');
+
+        if (video.paused) {
+            video.play()
+        }
+  })
+
+  video.addEventListener('click', function () {
+    if (video.paused) {
+        video.play()
+    } else {
+        videoWrapper.classList.add('video-overlay');
+        videoBtn.classList.remove('none');
+        video.pause();
+    }
+  })
+  
