4062e48f08913c871fd3279628ebbb9117c72468
Veretyushkina <toryray13@gmail.com>
2023-04-16 23:34:43 +0300

    done

--- js/main.js
+++ js/main.js
@@ -44,3 +44,17 @@ $(document).ready(function(){
     }
   })
   
+  /*mobile nav*/
+  const openNavbtn = document.querySelector('#openMobileNav');
+  const closeNavbtn = document.querySelector('#closeMobileNav');
+  const mobileNav = document.querySelector('#mobileNav');
+
+  openNavbtn.onclick = function(){
+    mobileNav.classList.remove('none')
+    document.body.classList.add('no-scroll')
+  }
+
+  closeNavbtn.onclick = function(){
+    mobileNav.classList.add('none')
+    document.body.classList.remove('no-scroll')
+  }
